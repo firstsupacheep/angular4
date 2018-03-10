5f80b67724b067395e5154eaefc814dc06a04341
firstsupacheep <supacheepSA@gmail.com>
2018-03-10 20:41:18 +0700

    install angular

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
+app.get('/hi', function (req, res) {
+  res.send('hi!')
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
